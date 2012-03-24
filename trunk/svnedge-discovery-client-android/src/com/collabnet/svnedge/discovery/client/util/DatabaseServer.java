56c1ace477bfca839b25b2266a7cace966440aa6
marcellodesales <marcello.desales@gmail.com>
2012-03-24 04:51:02 +0000

    Initial implementation of the Database server that will cache the previously discovered SVN servers.
    git-svn-id: http://csvn-ubuntu-32.local:18080/svn/android-projects@13 fdcd4cd3-8361-41e2-9dad-2dab825765a3

---
+++ trunk/svnedge-discovery-client-android/src/com/collabnet/svnedge/discovery/client/util/DatabaseServer.java
@@ -0,0 +1,44 @@
+package com.collabnet.svnedge.discovery.client.util;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseServer extends SQLiteOpenHelper {
+
+    private static final String TAG = "SVNEDGE_DB";
+    private static final String DB_NAME = "svnedge-servers.db";
+    private static final int DB_VERSION = 1;
+    
+    private static final String TABLE_NAME = "server";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_URL = "url";
+    private static final String COLUMN_CPU = "cpu";
+    private static final String COLUMN_OS = "os";
+    private static final String COLUMN_FOUND_ON = "found_on";
+
+    public DatabaseServer(Context context, String name, CursorFactory factory, int version) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE " + TABLE_NAME + " (" +
+            COLUMN_ID + " integer not null primary key autoincrement, " +
+            COLUMN_URL + " varchar not null, " +
+            COLUMN_CPU + " varchar not null, " +
+            COLUMN_OS + " varchar not null, " +
+            COLUMN_FOUND_ON + " long not null)";
+        Log.d(TAG, "onCreate=" + sql);
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
